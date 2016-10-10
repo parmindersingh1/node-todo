b2d8737e68441e7a289ccce0988d04022dfff4db
Parminder Singh <parminder@ezzie.in>
2016-10-10 09:48:46 +0530

    first commit

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -30,7 +30,7 @@ app.get('/todo', function(req, res) {
   Todo.find({}, function(err, data) {
     if(err) throw err;
     res.render('todo',{todos: data});
-  })
+  });
 });
 
 app.post('/todo',urlencodedParser, function(req, res) {
@@ -38,7 +38,7 @@ app.post('/todo',urlencodedParser, function(req, res) {
   var newTodo = Todo(req.body).save(function(err, data){
       if(err) throw err;
       res.json(data);
-  })
+  });
 
   // data.push(req.body);
   // res.json(data);
@@ -49,7 +49,7 @@ app.delete('/todo/:item', function(req, res) {
   Todo.find({item: req.params.item.replace(/\-/g," ")}).remove(function(err, data) {
     if(err) throw err;
     res.json(data);
-  })
+  });
   // data = data.filter(function(todo){
   //   return todo.item.replace(/ /g,'-') !== req.params.item;
   // });
